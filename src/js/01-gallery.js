b62f9badb2a58b748a0f34270f440dc82b956fe1
Yevhen Malyshko <memx86@gmail.com>
2022-01-07 10:34:02 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map(createGalleryItemMarkup).join('');
+
+galleryRef.innerHTML = galleryMarkup;
+
+const lightboxOptions = {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+let gallery = new SimpleLightbox('.gallery a', lightboxOptions);
+
+function createGalleryItemMarkup({ preview, original, description }) {
+  return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+}
