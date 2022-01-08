0faa155fa6052f193e4664e63bdd6c3822e0ac1d
Yevhen Malyshko <memx86@gmail.com>
2022-01-08 12:11:36 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const STORAGE_VIDEO_CURRENT_TIME = 'videoplayer-current-time';
+const throttledOnTimeUpdate = throttle(onTimeUpdate, 1000);
+const player = new Player('vimeo-player');
+
+if (localStorage[STORAGE_VIDEO_CURRENT_TIME]) {
+  player.setCurrentTime(localStorage[STORAGE_VIDEO_CURRENT_TIME]);
+}
+
+player.on('timeupdate', throttledOnTimeUpdate);
+
+function onTimeUpdate(e) {
+  localStorage.setItem(STORAGE_VIDEO_CURRENT_TIME, e.seconds);
+}
